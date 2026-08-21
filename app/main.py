f5b77759a33d5aca56bba1acf1827b0738984b34
pwork8296-bit <prateek2026dev@gmail.com>
2026-08-21 10:14:22 +0530

    allowed url

--- app/main.py
+++ app/main.py
@@ -24,7 +24,7 @@ api_router = APIRouter(prefix="/api/v1")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[frontend_url, "http://localhost:3000"],
+    allow_origins=[frontend_url, "http://localhost:3000",'https://blog-management-gold.vercel.app'],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
